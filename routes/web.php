668d55e74bf90e022023fdce0d304e8bb8991cca
chyngyzz85 <ar.chyngyz@gmail.com>
2023-10-31 12:27:03 +0600

    first commit

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+\Illuminate\Support\Facades\Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
